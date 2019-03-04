9c6c594d878df4cbb577bbdfa1cc08aea68b7e3b
Eric Kobrin <ekobrin@gatech.edu>
2019-03-04 07:00:36 -0500

    added comment for planning out how to do links

--- prototypes/js/grapher.js
+++ prototypes/js/grapher.js
@@ -82,6 +82,22 @@
     let gapf = 1;//0.03;
     let rf = 1;//0.08;
 
+    /* todo: consider defining subgraphs as a series of triples:
+      (i|p,i|p,idx) instead of sources and destingations
+      this takes us from:
+         /subgraph?sports=1110,1366,...&dports=1177,1433,...
+      to:
+         /pp54/pi234/ip3423/ii743
+      which says to first get idx=54 in a port/port matrix, then
+      idx=234 in the resulting port/ip matrix, then idx=2423 in
+      an ip/port view of that result, then finally to render the 743rd
+      element of an ip/ip view of that matrix.
+
+      What axes should be used for the last one? Not sure. Maybe use
+      the pi|pp|ip|ii pairs as a final dir entry?
+         e.g. /pp/54/pi/234/ip/3423/ii/743/pp to view the last
+         matrix as a port/port view by default?
+    */
     let showSubgraph = function([sps,dps]){
       console.log('subgraph?sports='+sps.join(',')+'&dports='+dps.join(','));
     }
