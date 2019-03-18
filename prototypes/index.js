d272bba8909fe6bc69b479622e0708be597543ab
Eric Kobrin <ekobrin@gatech.edu>
2019-03-18 07:21:49 -0400

    switched to using light wrapper around jsdom

--- prototypes/index.js
+++ prototypes/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-app.engine('html', require('express-views-dom')(app));
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+app.engine('html', function (filename, options, callback) {
+  JSDOM.fromFile(filename).then(dom => {
+    options || (options = { render: _ => null});
+    options.render(dom.window);
+    callback(null, dom.serialize());
+  });
+})
+
+app.set('view engine', 'html');
 
 
 const port = 3000;
@@ -12,7 +23,13 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/matrix/*', function(req, res){
   let ps = req.params['0'];
   let pp = me.pathParser(ps);
-  res.send('Hello World!')
+  res.render('matrix',{
+    render: function(window,done) {
+      let doc = window.document;
+      let t = doc.createTextNode(JSON.stringify(pp));
+      doc.getElementsByTagName("body")[0].appendChild(t);
+    }
+  });
 });
 
 app.listen(port, ip, () => console.log(`Example app listening on http://${ip}:${port}!`))
