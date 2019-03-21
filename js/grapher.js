14ad36c1451421c818b2472d1005a3bb9e121edd
Eric Kobrin <ekobrin@gatech.edu>
2019-03-21 06:19:49 -0400

    comment cleanup

--- js/grapher.js
+++ js/grapher.js
@@ -82,18 +82,13 @@
     let gapf = 1;//0.03;
     let rf = 1;//0.08;
 
-    /* Subgraphs are defined as a series of triples: (i|p,i|p,idx)
-       expressed as a path: /(i|p)(ip)/idx/...
-       e.g.  /pp/54/pi/234/ip/3423/ii/743
-      which says to first get idx=54 in a port/port matrix, then
-      idx=234 in the resulting port/ip matrix, then idx=2423 in
-      an ip/port view of that result, then finally to render the 743rd
-      element of an ip/ip view of that matrix.
+    /* Subgraphs are described in lib/matrixexplorer.js
 
-      What axes should be used for the last one? See the last path
-      element. If it is a number, the axes will be port/port. If it
-      is one of the pi|pp|ip|ii pairs, the axes expressed will be used.
-    */
+       The implementation here takes the current path and links
+       "index.html" in a virtual subdirectory whose relative
+       "pmatrix.js" will contain the specfic matrix information for
+       the selected index.
+     */
     let subgraphURL = function(idx){
       let newpath='./'+idx+'/pp/index.html'
       if(window.location.pathname.startsWith('/index.html')){
