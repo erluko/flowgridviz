e274b76354698276677c8116a51cc60999f4ee89
Eric Kobrin <ekobrin@gatech.edu>
2019-03-23 13:34:21 -0400

    reindented

--- js/filtermaker.js
+++ js/filtermaker.js
@@ -5,12 +5,12 @@
   var root = inNode?module.exports:(this.filtermaker={});
 
   root.tsDisplayFilter = function(sports,dports) {
-  sports = Array.from(sports);
-  dports = Array.from(dports);
-  return "(" +
-    sports.map(p=>'tcp.srcport == '+p + ' or udp.srcport == ' +p ).join(' or ') +
-    ') and (' +
-    dports.map(p=>'tcp.dstport == '+p + ' or udp.dstport == ' +p ).join(' or ') +
-    ')';
+    sports = Array.from(sports);
+    dports = Array.from(dports);
+    return "(" +
+      sports.map(p=>'tcp.srcport == '+p + ' or udp.srcport == ' +p ).join(' or ') +
+      ') and (' +
+      dports.map(p=>'tcp.dstport == '+p + ' or udp.dstport == ' +p ).join(' or ') +
+      ')';
   }
 })()
