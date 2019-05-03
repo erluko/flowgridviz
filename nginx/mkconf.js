37545a71d83f9ea5c3fcf4f3d09a027c5d53eb20
Eric Kobrin <ekobrin@gatech.edu>
2019-05-03 17:38:48 -0400

    enhanced mkconf.js to handle the two sorts of nginx installs seen so far. for #64

--- nginx/mkconf.js
+++ nginx/mkconf.js
@@ -2,6 +2,38 @@ let url_root = process.env.npm_package_config_url_root || '/';
 if(!url_root.endsWith("/")) url_root=url_root+'/';
 if(!url_root.startsWith("/")) url_root='/'+url_root;
 
+let fqdn =  process.env.npm_package_config_nginx_hostname;
+if(! fqdn ){
+  try {
+    let fs = require('fs');
+    fqdn = fs.readFileSync('/etc/mailname').toString().trim();
+  } catch(e){}
+}
+if(! fqdn ){
+  let os = require('os');
+  fqdn = os.hostname() || 'localhost';
+}
+
+let fullconf =  process.env.npm_package_config_nginx_full_config;
+if(fullconf && /no|false|null|disabled/i.test(fullconf)){
+   fullconf = false;
+}
+
+if(fullconf){
+  process.stdout.write(`server {
+  listen 80;
+  listen [::]:80;
+
+  server_name ${fqdn};
+
+  root /dev/null;
+  index /dev/null;
+
+  location / {
+    return 301 $scheme://$host${url_root};
+  }
+`);
+}
 process.stdout.write(`   location ${url_root} {
         proxy_pass http://${process.env.npm_package_config_listen_ip}:${process.env.npm_package_config_port};
         proxy_http_version 1.1;
@@ -11,3 +43,7 @@ process.stdout.write(`   location ${url_root} {
         proxy_cache_bypass $http_upgrade;
     }
 `);
+
+if(fullconf){
+  process.stdout.write('}\n');
+}
